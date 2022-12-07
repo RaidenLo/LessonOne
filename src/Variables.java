ac698f2d5935e45d03d54dc2814c98bb6fde5fa1
RaidenLo <raidenlo2837@gmail.com>
2022-12-07 17:05:50 -0600

    Finsh lesson starting working on problems

---
+++ src/Variables.java
@@ -0,0 +1,27 @@
+public class Variables {
+    public static void main(String[] args){
+
+        // datatype name = something;
+
+        int someInt = 9; // integer variable
+        long someLong = 500 // big number 
+        String someString = "text like this"; // sentances / text / anything not just one letter 
+        double somedouble = 0.1; //deciaml points values 
+        char achar = 'a'; // one letter / wont see often 
+        // true or false
+        boolean aBool =false; // or
+        boolean anotherBool = true;
+    
+        // operators
+
+        int math = 1+1 // add
+        math = 1*1
+        math = 1/1
+        math = 1 % 1; 
+        math++;
+        
+        math--;
+        
+        ExampleObject sObject = new ExampleObject("nice");
+    }
+}
